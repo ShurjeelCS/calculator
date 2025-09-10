0dfd5c76dba411ddd3cdce9d17c34d48ab2d3eaf
Shurjeel <shurjeel_m@hotmail.com>
2025-09-10 14:45:47 +0100

    added button pressing logic can now do calculations using buttons on calc.

--- script.js
+++ script.js
@@ -14,11 +14,6 @@ function divide(a, b) {
     return a/b;
 }
 
-console.log(add(5, 6));
-console.log(subtract(9, 6));
-console.log(multiply(5, 6));
-console.log(divide(10, 2));
-
 function operate(op, a, b) {
     switch (op) {
         case `+`:
@@ -38,4 +33,57 @@ function operate(op, a, b) {
     }
 }
 
-console.log(operate(`X` ,55, 11));
+//const display = document.querySelector(`.display`);
+const displayNum = document.querySelector(`#displayNum`);
+const buttons = document.querySelectorAll(`button`);
+let num1 = "";
+array =[];
+buttons.forEach(button => {
+    //display the text clicked button on screen
+    button.addEventListener(`click`, () => {
+        
+        
+        
+        if (button.textContent == `=`) {
+            array.push(displayNum.textContent);
+            let ans = operate(array[array.length - 3],parseInt(array[array.length - 2]),parseInt(array[array.length - 1]));
+            console.log("operations answer is " + ans);
+            displayNum.textContent = ans;
+            num1="";
+            array=[];
+        }else if (button.textContent == `AC`||button.textContent == `C`) {
+            displayNum.textContent = "";
+            num1="";
+            array=[];
+        }
+        else if (button.textContent == `+` ||
+            button.textContent == `-` || 
+            button.textContent == `X` || 
+            button.textContent == `/`  ) {
+                //calculation with multiple operators
+                if (array.length > 0) {
+                    array.push(displayNum.textContent);
+                    let ans = operate(array[array.length - 3],parseInt(array[array.length - 2]),parseInt(array[array.length - 1]));
+                    console.log("operations answer is " + ans);
+                    displayNum.textContent = ans;
+                    array.push(button.textContent);
+                    array.push(ans);
+                    num1="";
+                    console.log(array);
+                } //first round of calculations 
+                else {
+                    array.push(button.textContent);
+                    array.push(displayNum.textContent);
+                    console.log(array);
+                    num1 = "";
+                }
+                
+        } else {   
+            
+            console.log(`num1 is initially : ${num1}`);
+            num1 = num1 + button.textContent;
+            displayNum.textContent = num1;
+            console.log(`now doing number, num1 is initially : ${num1}`);
+        }
+    });
+});
